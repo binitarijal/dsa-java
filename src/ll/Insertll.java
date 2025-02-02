dda451fb819c72e7e39a9f415235c5a79668cbe9
Binita rijal <rijalbinita01@gmail.com>
2025-02-02 11:11:47 +0545

    insert at first linkede list

--- src/ll/Insertll.java
+++ src/ll/Insertll.java
@@ -25,6 +25,16 @@ public class Insertll {
             }
             tail = temp;
         }
+        void insertAtFirst(int b){
+            Node temp=new Node(b);
+            if(head==null){
+                head=tail=temp;
+            }
+            else{
+                temp.next=head;
+                head=temp;
+            }
+        }
 
         void display() {
             Node temp = head;
@@ -41,6 +51,11 @@ public class Insertll {
         ll.insertAtEnd(5);
         ll.insertAtEnd(6);
         ll.insertAtEnd(8);
+        ll.display();
+        System.out.println();
+        ll.insertAtEnd(9);
+        ll.insertAtEnd(2);
+        ll.insertAtFirst(1);
         ll.display();
 
     }
