48f97bc9b32839281b488b4e7d3d819ca291d20f
Binita rijal <rijalbinita01@gmail.com>
2025-02-16 08:21:16 +0545

    new array qn

--- src/collectionframework.java
+++ src/collectionframework.java
@@ -1,13 +1,28 @@
 import java.util.Arrays;
 
 public class collectionframework {
-    public static void main(String[] args) {
 
-int arr[]={1,2,3,4,5,6,7};
-int empt[]=new int[arr.length];
-for(int i=arr.length-1,j=0;i>=0;i--,j++){
-    empt[j]=arr[i];
-}
-        System.out.println(Arrays.toString(empt));
+    static int printSum(int[] arr){
+        int sum=0;
+        int len= arr.length;
+        for(int i=0;i< arr.length;i++){
+            sum+=arr[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int a[]={1,2,3,4};
+        System.out.println(printSum(a));
+
+
+//int arr[]={1,2,3,4,5,6,7};
+//int empt[]=new int[arr.length];
+//for(int i=arr.length-1,j=0;i>=0;i--,j++){
+//    empt[j]=arr[i];
+//}
+//        System.out.println(Arrays.toString(empt));
+
+
     }
 }
